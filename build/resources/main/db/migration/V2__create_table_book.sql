baa5642bcafac88dbbbf4efdd80fb99b4c365888
Carlos Eduardo Augusto Aguiar <carlos.aguiar@engdb.com.br>
2021-12-22 22:14:09 -0300

    Remove /build folder

--- build/resources/main/db/migration/V2__create_table_book.sql
+++
@@ -1,9 +0,0 @@
-create table book (
-    id int auto_increment primary key,
-    name varchar(255) not null,
-    price decimal(10,2) not null,
-    status varchar(255) not null,
-    customer_id int not null,
-
-    FOREIGN KEY (customer_id) REFERENCES customer(id)
-)
